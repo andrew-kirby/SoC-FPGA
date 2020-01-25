efec228d0c76d97bac66bd4c43a874074bca1efd
Cameron Blegen <c.bleg98@gmail.com>
2020-01-25 16:14:37 -0700

    HW 4 done + skeleton of projet

---
+++ LAB1/ModelSim_VHDL/rsqrt.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity rsqrt is
+
+	generic (W_bits		: positive := 16;
+		 F_bits		: positive := 8;
+		 N_iterations	: positive := 3);
+
+	port	(clk : in  std_logic;
+		 x   : in  std_logic_vector(W_bit-1 downto 0);
+		 y   : out std_logic_vector(W_bit-1 downto 0));
+
+end entity rsqrt;
+
+architecture rsqrt_arch of rsqrt is
+
+begin
+
+end rsqrt_arch;
