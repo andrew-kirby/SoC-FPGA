b870fb00bc77e9aaa5da0fb47116992c91ab12d1
andrew-kirby <andy-kirby@live.com>
2020-02-02 18:51:53 -0700

    rsqrt and compute_y0 close-ish

--- LAB1/ModelSim_VHDL/AndyModelSim/newton_iter.vhd
+++ LAB1/ModelSim_VHDL/AndyModelSim/newton_iter.vhd
@@ -5,8 +5,7 @@ use ieee.numeric_std.all;
 entity newton_iter is
 
 	generic (W_bits		: positive := 16;
-		 F_bits		: positive := 8;
-		 N_iterations	: positive := 3);
+		 F_bits		: positive := 8);
 
 	port	(clk  	: in  std_logic;
 		 x    	: in  std_logic_vector(W_bits-1 downto 0);
