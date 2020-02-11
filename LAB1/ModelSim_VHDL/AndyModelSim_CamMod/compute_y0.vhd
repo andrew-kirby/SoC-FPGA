1f7bd7bccbd062bc84f6b2e7e6f287f645885785
Cameron Blegen <c.bleg98@gmail.com>
2020-02-11 14:28:06 -0700

    Finished Test bench and debugging

--- LAB1/ModelSim_VHDL/AndyModelSim_CamMod/compute_y0.vhd
+++ LAB1/ModelSim_VHDL/AndyModelSim_CamMod/compute_y0.vhd
@@ -41,22 +41,26 @@ constant two_exp : unsigned(15 downto 0) := "1011010100000100";
 signal x1 	: std_logic_vector(W_bits-1 downto 0);
 signal x2 	: std_logic_vector(W_bits-1 downto 0);
 signal x3 	: std_logic_vector(W_bits-1 downto 0);
-signal x4 	: std_logic_vector((W_bits*2)-1 downto 0);
-signal Z 	: std_logic_vector(W_bits-1 downto 0);
+signal x4 	: std_logic_vector(W_bits-1 downto 0);
+signal x5 	: std_logic_vector(W_bits-1 downto 0);
+signal x6 	: std_logic_vector(W_bits-1 downto 0);
+signal Z 	: std_logic_vector(3 downto 0); --HAVE TO CHANGE IF REGENERATED
 signal Z_int	: integer;
 signal B_int 	: integer;
 signal B2_int 	: integer;
 signal B3_int 	: integer;
 signal B4_int 	: integer;
+signal B5_int 	: integer;
 signal A_int	: integer;
 signal x_a	: std_logic_vector(W_bits-1 downto 0);
 signal x_a2	: std_logic_vector(W_bits-1 downto 0);
+signal x_a3	: std_logic_vector(W_bits-1 downto 0);
 signal x_b	: std_logic_vector(W_bits-1 downto 0);
-signal y0_big   : std_logic_vector((W_bits+13)-1 downto 0); --13 from LUT value output bits
+signal y0_big   : std_logic_vector((W_bits+16+13)-1 downto 0); --13 from LUT value output bits 16 from the potential of 2^1/2 fraction
 
 -- LUT SIGNALS --
-signal num_in	: std_logic_vector(W_bits-1 downto 0);
-signal x_b_exp	: std_logic_vector(W_bits-1 downto 0);
+signal num_in	: std_logic_vector(7 downto 0); -- hard coded due to LUT premade
+signal x_b_exp	: std_logic_vector(12 downto 0);
 
 begin
 
@@ -84,32 +88,36 @@ begin
 			B_int <= W_bits - F_bits - Z_int - 1;
 			-- Compute Alpha
 			x3 <= x2;
+			B2_int <= B_int;
 			if((B_int mod 2) = 0) then -- B even
 				A_int <= (-2 * B_int) + (B_int / 2); --always negative
 			else -- B odd
 				A_int <= -2*B_int + B_int/2 + 1; --always negative
 			end if;
 			-- Compute Xa and Xb
-			x4(W_bits-1 downto 0) <= x3; -- made x4 bigger so that it can be used for the left shift
-			B2_int <= B_int;
-			-- Andy, I think these two below can be replaced by what I put in place
-				-- x_a <= std_logic_vector( unsigned(x4) * shift_left(to_unsigned(1, W_bits), (F_bits + A_int)) );
-				-- x_b <= std_logic_vector( unsigned(x4) * shift_left(to_unsigned(1, W_bits), (F_bits + A_int)) );
-			-- but I could be wrong so check me. Look at 4 & 5 on the lab handout
+			x4 <= x3;
+			B3_int <= B2_int;
 			x_a <= std_logic_vector(shift_left(unsigned(x4), A_int)); -- replaced previous -- only need the fractional bits so don't need to worry about size, radix stays in same place
 			x_b <= std_logic_vector(shift_right(unsigned(x4), B_int)); -- replaced previous -- only need the fractional bits so don't need to worry about size, radix stays in same place
-			B3_int <= B2_int;
-			-- Use lookup table
-			num_in <= std_logic_vector(x_b(F_bits-1 downto F_bits-8)); -- only need the fractional bits so don't need to worry about size, radix stays in same place
+			-- Use lookup table to get XB to the -3/2
+			--! need to append 0s if F < 8
+			x5 <= x4;
 			B4_int <= B3_int;
 			x_a2 <= x_a;
+			num_in <= std_logic_vector(x_b(F_bits-1 downto F_bits-8)); -- only need the fractional bits so don't need to worry about size, radix stays in same place
+			-- compute Y0
+			x6 <= x5;
+			B5_int <= B4_int;
+			x_a3 <= x_a2;
 			if((B4_int mod 2) = 0) then -- B even
-			    y0_big <= std_logic_vector(unsigned(x_b_exp) * unsigned(x_a2)); -- this step adds 12 fractional bits
-			    y0     <= y0_big(W_bits+11 downto F_bits+11); -- +11 because 12-1
+			    y0_big(y0_big'length-1 downto y0_big'length-W_bits-13) <= std_logic_vector(unsigned(x_b_exp) * unsigned(x_a2)); -- this step adds 12 fractional bits
 			else -- B odd
-			    y0_big <= std_logic_vector(unsigned(x_b_exp) * unsigned(x_a2) * two_exp); -- this step adds 12 fractional bits
-			    y0     <= y0_big(W_bits+11 downto F_bits+11);
-			end if;						
+			    y0_big <= (std_logic_vector(unsigned(x_b_exp) * unsigned(x_a2) * two_exp)); -- this step adds 12 fractional bits
+			end if;				
+
+			y0 <= y0_big(y0_big'length-1 downto y0_big'length-W_bits);
+			x_out <= x6;
+
 
 		end if;		
 	end process;
--- LAB1/ModelSim_VHDL/AndyModelSim_CamMod/rsqrt_tb.vhd
+++ LAB1/ModelSim_VHDL/AndyModelSim_CamMod/rsqrt_tb.vhd
@@ -13,7 +13,7 @@ component rsqrt is
 
 	generic (W_bits		: positive := 16;
 		 F_bits		: positive := 8;
-		 N_iterations	: positive := 3);
+		 N_iterations	: positive := 16);
 
 	port	(clk : in  std_logic;
 		 x   : in  std_logic_vector(W_bits-1 downto 0);
@@ -27,14 +27,13 @@ end component rsqrt;
 file file_VECTORS : text;
 file file_OUTPUT  : text;
 
-constant W 	: natural := 8;
+constant W 	: natural := 16;
 constant F 	: natural := 8;
-constant WIDTH 	: natural := W + F;
-constant N 	: natural := 8;
+constant N 	: natural := 16;
 
 signal clk 	: std_logic := '0';
-signal num_in	: std_logic_vector(WIDTH-1 downto 0);
-signal num_out	: std_logic_vector(WIDTH-1 downto 0);
+signal num_in	: std_logic_vector(W-1 downto 0);
+signal num_out	: std_logic_vector(W-1 downto 0);
 
 begin
 
@@ -55,20 +54,20 @@ port map (
 stimulus : process
 	variable input_line	: line;
 	variable output_line	: line;
-	variable input_num	: std_logic_vector(WIDTH-1 downto 0);
+	variable input_num	: std_logic_vector(W-1 downto 0);
 
 	begin
 
 	clk <= '0'; -- Set the clock low
 
-	file_open(file_VECTORS, "input", read_mode);
+	file_open(file_VECTORS, "input.txt", read_mode);
 	file_open(file_OUTPUT, "output", write_mode);
 
 	while not endfile(file_VECTORS) loop
 		-- Read in a line from the file
 		readline(file_VECTORS, input_line);
 		read(input_line, input_num);
-		-- Assign the input number to the signal linked to the adder
+		-- Assign the input number to the signal
 		num_in <= input_num;
 
 		wait for 10 ns;
